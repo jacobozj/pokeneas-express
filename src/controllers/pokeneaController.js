fd633ff8f941ae1d2e38a1d77e5ae038613bf267
Jacobo Zuluaga Jaramillo <104317349+jacobozj@users.noreply.github.com>
2024-11-12 20:34:32 -0500

    Update pokeneaController.js

--- src/controllers/pokeneaController.js
+++ src/controllers/pokeneaController.js
@@ -4,7 +4,7 @@ const pokeneas = require('../models/pokeneaModel');
 
 const getRandomPokenea = (req, res) => {
     const randomPokenea = pokeneas[Math.floor(Math.random() * pokeneas.length)];
-    res.json({ id: randomPokenea.id, name: randomPokenea.name, height: randomPokenea.height, ability: randomPokenea.ability, containerId: process.env.CONTAINER_ID });
+    res.json({ id: randomPokenea.id, name: randomPokenea.name, height: randomPokenea.height, ability: randomPokenea.ability, containerId: os.hostname() });
 };
 
 const getPokeneaImageAndPhilosophy = (req, res) => {
